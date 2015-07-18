32cbd3919db93e7cc121a0d752ca95fc90202d82
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2015-07-18 17:06:32 +0200

    Added lazy format and eff parsing

--- src/main/scala/org/kornel/vcf/ParseAnnotated.scala
+++ src/main/scala/org/kornel/vcf/ParseAnnotated.scala
@@ -1,57 +1,81 @@
 package org.kornel.vcf
 
+import scala.util.Try
+
 object ParseAnnotated extends App {
 
   import Parse._
-  import Format._
-  import EFFFormat._
 
   val inputPath = "/Users/kornel.kielczewski/maize/AGPv3.fasta.mp.bcf.MUT.filt.annotated.vcf"
 
   val file = scala.io.Source.fromFile(inputPath)
 
-  val lines = skipHeader(file.getLines()).map(_.split("\t"))
-    .filter(x => chromosome(x) == "10")
+  val lines = skipHeader(file.getLines()).flatMap(line => VC(line.split("\t")))
+    .filter(_.chromosome == 456)
+    .flatMap(_.format().eff.map(_.name))
 
-  val x = format _ andThen parseFields andThen extractEff andThen parseEff
+  lines.toSeq.distinct.foreach(println)
+}
 
-  lines.map(x).toSeq.distinct.foreach(println)
+case class EFF(name: String, content: String)
 
+case class Format(eff: Stream[EFF])
+
+object Format {
+
+  private def parseFields(fields: String) = fields.split(";").map {
+    x => val split = x.split("=")
+      if (split.length > 1) split(0) -> split(1) else split(0) -> ""
+  }.toMap
+
+  private def extractEff(fields: Map[String, String]) = fields("EFF")
+
+  private def parseEffs(field: String): Stream[EFF] = {
+    val effs = Stream(field.split(","): _*)
+    effs.map {
+      effRaw =>
+        val contentStart = effRaw.indexOf('(')
+        val name = effRaw.substring(0, contentStart)
+        val content = effRaw.substring(contentStart, effRaw.length - 1)
+        print(".")
+        EFF(name, content)
+    }
+  }
+
+  def apply(raw: String): Format = {
+    Format(parseEffs(extractEff(parseFields(raw))))
+  }
+}
+
+case class VC(chromosome: Int, position: Int, format: () => Format)
+
+object VC {
+
+  def extractChromosome(fields: Array[String]) = fields(0)
+
+  def extractPosition(fields: Array[String]) = fields(1)
+
+  def extractFormat(fields: Array[String]) = fields(7)
+
+  //  def hds1(fields: Array[String]) = fields(8)
+  //
+  //  def hds2(fields: Array[String]) = fields(9)
+
+  def apply(tokens: Array[String]): Option[VC] = {
+    Try(extractChromosome(tokens).toInt).toOption.map {
+      chromosome =>
+        val position = extractPosition(tokens).toInt
+        val format = () => Format(extractFormat(tokens))
+        VC(chromosome, position, format)
+    }
+  }
 
 }
 
 object Parse {
 
+
   def skipHeader(lines: Iterator[String]) = lines.dropWhile(_.startsWith("#"))
 
-  def chromosome(fields: Array[String]) = fields(0)
-
-  def position(fields: Array[String]) = fields(1)
-
-  def format(fields: Array[String]) = fields(7)
-
-  def hds1(fields: Array[String]) = fields(8)
-
-  def hds2(fields: Array[String]) = fields(9)
 }
 
-object Format {
-
-  def parseFields(fields: String) = fields.split(";").map {
-    x => val split = x.split("=")
-      if (split.length > 1) split(0) -> split(1) else split(0) -> ""
-  }.toMap
-
-
-}
-
-object EFFFormat {
-
-  def extractEff(fields: Map[String, String]) = fields("EFF")
-
-  def parseEff(field: String) = {
-    val name = field.substring(0, field.indexOf("("))
-    name
-  }
-
-}
