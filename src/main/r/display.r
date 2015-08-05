a74d7bf87beadb26776c4cbcf4cae3412a7b5437
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2015-08-05 22:53:10 +0200

    Plots for all variants

--- src/main/r/display.r
+++ src/main/r/display.r
@@ -1,31 +1,36 @@
-data <- read.csv("frameshift_variant-positions.csv",
-                 sep=";", 
-                 header=FALSE, 
-                 col.names=c("Chrom", "Pos", "Value"))
-
 require(dplyr)
 require(ggplot2)
 require(scales)
 
-source("multiplot.r")
+plot.positions <- function(filename) {
+  
+  data <- read.csv("/tmp/ax/ax-frameshift_variant.csv",
+                   sep=",",
+                   header=FALSE, 
+                   col.names=c("Chrom", "Pos", "Value"))
+  number_ticks <- function(n) {function(limits) pretty(limits, n)}
+  
+  plots <- NULL
+  chrom.data <- NULL
+  
+  for (i in 1:10) {
+    chrom.data[[i]] <- filter(data, Chrom == i)
+    
+    p <- ggplot(chrom.data[[i]], aes(x=Pos, y=Value, color=Value)) + 
+      geom_point(shape=1) + 
+      geom_smooth(method="loess") +
+      scale_x_continuous(breaks=number_ticks(7)) +
+      ggtitle(paste(filename, "\nchromosome", i))
+    
+    ggsave(sprintf("plot%d.png", filename, i), p)
+    
+    plots[[i]] <- p
+  }
 
-plots <- NULL
-chrom.data <- NULL
-
-for (i in 1:10) {
-  chrom.data[[i]] <- filter(data, Chrom == i)
-  
-  p <- ggplot(chrom.data[[i]], aes(x=Pos, y=Value, color=Value)) + 
-    geom_point(shape=1) + 
-    scale_x_continuous(trans=log2_trans()) +
-    ggtitle(paste("chromosome", i))
-  
-  ggsave(sprintf("plot%d.png", i), p)
-  
-  plots[[i]] <- p
 }
 
-#multiplot(plotlist=plots[seq(1,3)])
-#multiplot(plotlist=plots[seq(4,6)])
-#multiplot(plotlist=plots[seq(7,10)])
+files <- list.files("/tmp/bx")
 
+for (f in files) {
+  plot.positions(sprintf("/tmp/bx/%s", f)) 
+}
