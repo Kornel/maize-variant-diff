9f02b3920d8d2687cf7b8032c905988b5a6531e6
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2015-08-06 21:27:44 +0200

    abline, and fixed y min/max value

--- src/main/r/display.r
+++ src/main/r/display.r
@@ -3,7 +3,7 @@ require(ggplot2)
 require(scales)
 require(tools)
 
-plot.positions <- function(filename) {
+plot.positions <- function(filename, overwrite=FALSE, doNotSave=FALSE) {
   
   print(sprintf("Working on %s", filename))
   
@@ -16,6 +16,8 @@ plot.positions <- function(filename) {
   plots <- NULL
   chrom.data <- NULL
   
+  yscale = max(abs(min(data$Value)), max(data$Value))
+  
   for (i in 1:10) {
     chrom.data[[i]] <- filter(data, Chrom == i)
     
@@ -23,7 +25,7 @@ plot.positions <- function(filename) {
     
     output.filename <- sprintf("plots/%s/plot%.2d.png", variant.type, i)
     
-    if (file.exists(output.filename)) {
+    if (!overwrite && file.exists(output.filename) && !doNotSave) {
       print(sprintf("File %s already exists, skipping...", output.filename))
       next
     }
@@ -31,27 +33,38 @@ plot.positions <- function(filename) {
     title <- paste(variant.type, "\n", "Chromosome", i)
     
     tryCatch( {
+      
       p <- ggplot(chrom.data[[i]], aes(x=Pos, y=Value, color=Value)) + 
         geom_point(shape=1) + 
         geom_smooth(method="loess") +
+        geom_abline(slope = 0, intercept = 0, colour = 'red', alpha = 0.3) + 
         ylab("hds1 - hds2") + 
+        ylim(-yscale, yscale) + 
         xlab("Position") +
         scale_x_continuous(breaks=number_ticks(7)) +
         ggtitle(title)
+
       
-      dir.create(sprintf("plots/%s/", variant.type))
-      ggsave(output.filename, p)
+      if (!doNotSave) {
+        dir.create(sprintf("plots/%s/", variant.type))
+        print(sprintf("Creating plot %s", output.filename))
+        ggsave(output.filename, p)
+      }
       
       plots[[i]] <- p
     }, error = function(e) {
       print(e$message)
     })
   }
+  
+  return(plots)
 
 }
 
 files <- list.files("variants")
 
 for (f in files) {
-  plot.positions(sprintf("variants/%s", f)) 
+  plot.positions(sprintf("variants/%s", f), overwrite=TRUE) 
 }
+
+# plot.positions("variants/frameshift_variant.csv", doNotSave=TRUE)
